0527ff074d73a2dd36bbe8dcf92046ae547386f6
Mauro Pretti <mauspretti@gmail.com>
2023-06-05 21:47:52 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,16 +3,49 @@
  * Copyright 2019 Google LLC. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
-// [START maps_map_simple]
 let map;
+const Chicago = { lat: -34.5822599, lng: -58.4428659 };
+
+/**
+ * Creates a control that recenters the map on Chicago.
+ */
+function createCenterControl(map) {
+  const controlButton = document.createElement("button");
+
+  // Set CSS for the control.
+  controlButton.classList.add('buttonStyle');
+
+  controlButton.textContent = "Center Map";
+  controlButton.title = "Click to recenter the map";
+  controlButton.type = "button";
+  // Setup the click event listeners: simply set the map to Chicago.
+  controlButton.addEventListener("click", () => {
+    map.setCenter(argentina);
+  });
+  return controlButton;
+}
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 123.644},
+    zoom: 8
   });
+
+  var georssLayer = new google.maps.KmlLayer({
+    url: "http://api.flickr.com/services/feeds/geo/?g=322338@N20&lang=en-us&format=feed-georss",
+  });
+  georssLayer.setMap(map);
+
+  // Create the DIV to hold the control.
+  const centerControlDiv = document.createElement("div");
+  // Create the control.
+  const centerControl = createCenterControl(map);
+
+  // Append the control to the DIV.
+  centerControlDiv.appendChild(centerControl);
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(
+    centerControlDiv
+  );
 }
 
 window.initMap = initMap;
-
-// [END maps_map_simple]
